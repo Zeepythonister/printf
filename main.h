7ccc4b3d1a9fd4c66a54e5cb991ac38c39ee2ce3
Zeepythonister <zeepythonister@gmail.com>
2022-04-18 05:35:39 -0700

    include test file

--- main.h
+++ main.h
@@ -1,10 +1,10 @@
 #ifndef PRINT_F
-#define PRINT_f
+#define PRINT_F
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdarg.h"
+#include "unistd.h"
 
 int _putchar(int c);
 #endif
